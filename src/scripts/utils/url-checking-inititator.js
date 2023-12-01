a19e574ddb63b3bc3ba6f84f080a11bd7f235105
tangkasR <tangkasrisdianto2@gmail.com>
2023-12-01 14:32:58 +0700

    updated app js

---
+++ src/scripts/utils/url-checking-inititator.js
@@ -0,0 +1,97 @@
+import Swal from "sweetalert2";
+
+export const checkUrl = (url, isLogin) => {
+  if (
+    url === "/dashboard" ||
+    url === "/profil" ||
+    url === "/wisata" ||
+    url === "/editprofil" ||
+    url === "/addwisata" ||
+    url === "/editwisata/:id" ||
+    url === "/hotel" ||
+    url === "/addhotel" ||
+    url === "/edithotel/:id" ||
+    url === "/galleryadminbyid/:id" ||
+    url === "/galleryadmin" ||
+    url === "/gallerywisataadmin/:id"
+  ) {
+    if (isLogin === true) {
+      // set localstorage to remove
+      window.onbeforeunload = () => {
+        return localStorage.removeItem("id");
+      };
+      // end set localstorage to remove
+
+      // menonaktifkan navbar user
+      const navbar = document.querySelector("navbar-element");
+      navbar.style.display = "none";
+
+      // hapus footer
+      const footer = document.querySelector("footer-element");
+      footer.style.display = "none";
+
+      return url;
+    } else {
+      Swal.fire({
+        imageUrl: `./icons/icon-bingung.png`,
+        imageWidth: 200,
+        imageAlt: "Custom image",
+        title: `<h5 style="font-weight:800;color:red;font-size:30px">ANDA BELUM LOGIN!</h5>`,
+        text: `Login terlebih dahulu!`,
+        showConfirmButton: false
+      });
+      return (url = "/login");
+    }
+  }
+  if (
+    url === "/" ||
+    url === "/home" ||
+    url === "/gallery" ||
+    url === "/editprofil" ||
+    url === "/contact" ||
+    url === "/map" ||
+    url === "/kategorialam" ||
+    url === "/kategoribudaya" ||
+    url === "/login" ||
+    url === "/register" ||
+    url === "/favoritwisata" ||
+    url === "/detail/:id" ||
+    url === "/favorithotel" ||
+    url === "/detailhotel/:id" ||
+    url === "/detailgallery/:id" ||
+    url === "/hotellandingpage"
+  ) {
+    if (isLogin === true) {
+      return (url = "/dashboard");
+    } else {
+      //nonactive navbar
+      if (url == "/login" || url == "/register") {
+        const navLink = document.querySelectorAll(".nav-link");
+        navLink.forEach((link) => {
+          link.classList.remove("active");
+        });
+      }
+      //end nonactive navbar
+
+      // mengaktifkan navbar
+      const navbar = document.querySelector("navbar-element");
+      navbar.style.display = "block";
+
+      // mengaktifkan footer
+      const footer = document.querySelector("footer-element");
+      footer.style.display = "block";
+
+      if (url === "/") {
+        // nonaktifkan navbar
+        const navbar = document.querySelector("navbar-element");
+        navbar.style.display = "none";
+
+        // nonaktifkan footer
+        const footer = document.querySelector("footer-element");
+        footer.style.display = "none";
+      }
+      return url;
+    }
+  }
+  return (url = "");
+};
--- src/scripts/views/app.js
+++ src/scripts/views/app.js
@@ -1,165 +1,19 @@
-import UrlParser from '../routes/url-parser';
-import routes from '../routes/routes';
-import {middleware} from '../middleware/middleware';
-import Swal from 'sweetalert2';
-import SidebarInitiator from '../utils/sidebar-initiator';
+import UrlParser from "../routes/url-parser";
+import routes from "../routes/routes";
+import { middleware } from "../middleware/middleware";
+import { checkUrl } from "../utils/url-checking-inititator";
+import SidebarInitiator from "../utils/sidebar-initiator";
+
 class App {
-  constructor({content}) {
+  constructor({ content }) {
     this._content = content;
   }
-  async renderPage () {
-    let url = UrlParser.parseActiveUrlWithCombiner ();
-    const isLogin = await middleware ();
-    if (
-      url === '/dashboard' ||
-      url === '/profil' ||
-      url === '/wisata' ||
-      url === '/editprofil' ||
-      url === '/addwisata' ||
-      url === '/editwisata/:id' ||
-      url === '/hotel' ||
-      url === '/addhotel' ||
-      url === '/edithotel/:id' ||
-      url === '/galleryadminbyid/:id' ||
-      url === '/galleryadmin' ||
-      url === '/gallerywisataadmin/:id'
-    ) {
-      if (isLogin === true) {
-        // set localstorage to remove
-        window.onbeforeunload = () => {
-          return localStorage.removeItem ('id');
-        };
+  async renderPage() {
+    let url = UrlParser.parseActiveUrlWithCombiner();
+    const isLogin = await middleware();
+    const urlTo = checkUrl(url, isLogin);
 
-        // end set localstorage to remove
-
-        const page = routes[url];
-        this._content.innerHTML = await page.render ();
-        await page.afterRender ();
-        // menonaktifkan navbar user
-        const navbar = document.querySelector ('navbar-element');
-        navbar.style.display = 'none';
-
-        // hapus footer
-        const footer = document.querySelector ('footer-element');
-        footer.style.display = 'none';
-
-        // akses side bar
-
-        SidebarInitiator.init ({
-          button: document.querySelector ('.btn-sidebar'),
-          drawer: document.querySelector ('.sidebar'),
-          content: document.getElementById ('content_container'),
-        });
-      } else {
-        Swal.fire ({
-          imageUrl: `./icons/icon-bingung.png`,
-          imageWidth: 200,
-          imageAlt: 'Custom image',
-          title: `<h5 style="font-weight:800;color:red;font-size:30px">ANDA BELUM LOGIN!</h5>`,
-          text: `Login terlebih dahulu!`,
-          showConfirmButton: false,
-        });
-        if (
-          url === '/dashboard' ||
-          url === '/profil' ||
-          url === '/wisata' ||
-          url === '/editprofil' ||
-          url === '/addwisata' ||
-          url === '/editwisata/:id' ||
-          url === '/hotel' ||
-          url === '/addhotel' ||
-          url === '/edithotel/:id' ||
-          url === '/galleryadminbyid/:id' ||
-          url === '/galleryadmin' ||
-          url === '/gallerywisataadmin/:id'
-        ) {
-          window.location.replace ('#/login');
-        }
-        url = UrlParser.parseActiveUrlWithCombiner ();
-        const page = routes[url];
-        this._content.innerHTML = await page.render ();
-        await page.afterRender ();
-      }
-    } else if (
-      url === '/' ||
-      url === '/home' ||
-      url === '/gallery' ||
-      url === '/editprofil' ||
-      url === '/contact' ||
-      url === '/map' ||
-      url === '/kategorialam' ||
-      url === '/kategoribudaya' ||
-      url === '/login' ||
-      url === '/register' ||
-      url === '/favoritwisata' ||
-      url === '/detail/:id' ||
-      url === '/favorithotel' ||
-      url === '/detailhotel/:id' ||
-      url === '/detailgallery/:id' ||
-      url === '/hotellandingpage'
-    ) {
-      if (isLogin === false) {
-        //active navbar
-        if(url == '/login' || url=='/register'){
-          const navLink = document.querySelectorAll ('.nav-link');
-          navLink.forEach (link => {
-            link.classList.remove ('active');
-          });
-        }
-        //end active navbar
-
-        const page = routes[url];
-        if (page !== undefined) {
-          if (url === '/') {
-            // nonaktifkan navbar
-            const navbar = document.querySelector ('navbar-element');
-            navbar.style.display = 'none';
-
-            // nonaktifkan footer
-            const footer = document.querySelector ('footer-element');
-            footer.style.display = 'none';
-            this._content.innerHTML = await page.render ();
-            await page.afterRender ();
-            return;
-          }
-          this._content.innerHTML = await page.render ();
-          await page.afterRender ();
-          // mengaktifkan navbar
-          const navbar = document.querySelector ('navbar-element');
-          navbar.style.display = 'block';
-
-          // mengaktifkan footer
-          const footer = document.querySelector ('footer-element');
-          footer.style.display = 'block';
-          return;
-        }
-      } else {
-        if (
-          url === '/' ||
-          url === '/home' ||
-          url === '/gallery' ||
-          url === '/editprofil' ||
-          url === '/contact' ||
-          url === '/map' ||
-          url === '/kategorialam' ||
-          url === '/kategoribudaya' ||
-          url === '/login' ||
-          url === '/register' ||
-          url === '/favoritwisata' ||
-          url === '/detail/:id' ||
-          url === '/favorithotel' ||
-          url === '/detailhotel/:id' ||
-          url === '/hotellandingpage' ||
-          url === '/detailgallery/:id'
-        ) {
-          window.location.replace ('#/dashboard');
-        }
-        url = UrlParser.parseActiveUrlWithCombiner ();
-        const page = routes[url];
-        this._content.innerHTML = await page.render ();
-        await page.afterRender ();
-      }
-    } else {
+    if (urlTo === "") {
       this._content.innerHTML += `
         <div
           class="d-flex align-items-center flex-column justify-content-center"
@@ -169,6 +23,19 @@ class App {
           <p style="font-size:20px; font-weight:600">Not Found</p>
         </div>
       `;
+      return;
+    }
+
+    const page = routes[urlTo];
+    this._content.innerHTML = await page.render();
+    await page.afterRender();
+    if (urlTo === "/dashboard") {
+      // akses side bar
+      SidebarInitiator.init({
+        button: document.querySelector(".btn-sidebar"),
+        drawer: document.querySelector(".sidebar"),
+        content: document.getElementById("content_container")
+      });
     }
   }
 }
